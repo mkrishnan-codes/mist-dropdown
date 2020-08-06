6909f215af397c415c5a5051441991987af3f1ab
mkrishnan-codes <manu.krishnan@tarams.com>
2020-08-06 15:43:02 +0530

    api added as source

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ const getData = async () => {
   return airports['default'];
 };
 const getOtherData = async () => {
-  const airports = await import('./configs/airports-2.js');
-  return airports['default'];
+  try {
+    const response = await fetch('https://manu-large-data.herokuapp.com/get-xxl');
+    return response.json();
+  } catch (e) {
+    console.log(e)
+    return [];
+  }
+
 };
 
 function App() {
@@ -23,6 +29,7 @@ function App() {
       <div className="airport-container">
         {!large ? <AirportChooser
           className="airport-chooser"
+          key="small"
           getAsyncData={getData}
           keyExtractor={item => item.code}
           itemRender={item => <Airport name={item.name} city={item.city} country={item.country} code={item.code} />}
@@ -33,19 +40,19 @@ function App() {
         />
           :
           <AirportChooser
-           
+            key="large"
             getAsyncData={getOtherData}
-            keyExtractor={item => item.code}
-            itemRender={item => `${item.name}`}
-            label="Select airport"
-            onSelect={item => setLabel(`${item.name} - ${item.code}`)}
-            filterFn={(item, filterValue) => item['name'].toLowerCase().search(filterValue.replace(/\\/g, "\\\\")) > -1}
+            keyExtractor={item => item.id}
+            itemRender={item => `${item.words}`}
+            label="Select words"
+            onSelect={item => setLabel(`${item.words}`)}
+            filterFn={(item, filterValue) => item['words'].toLowerCase().search(filterValue.replace(/\\/g, "\\\\")) > -1}
             loaderRenderFn={() => <Loader />}
           />
         }
         <div className="sel-item">
           <div>
-            Try another data
+            Try large data from api
           </div>
           <input type="checkbox" value={large} onChange={(e) => setLarge(e.target.checked)} />
         </div>
